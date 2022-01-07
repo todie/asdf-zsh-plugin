4541b6b384e778ad480c6d8febbd856a2b306a4e
Christian Todie <christian.todie@gmail.com>
2022-01-07 01:23:05 -0500

    ensure plugins are loaded

--- asdf.plugin.zsh
+++ asdf.plugin.zsh
@@ -1,8 +1,23 @@
-ASDF_DIR=$XDG_DATA_HOME/asdf
-ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdfrc
-ASDF_DATA_DIR=$XDG_CACHE_HOME/asdf
+export ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdfrc
+export ASDF_DIR=$XDG_DATA_HOME/asdf
+export ASDF_DATA_DIR=$XDG_CACHE_HOME/asdf
+
+function zsh_plugin:asdf:tools () {
+  awk '{ print $1 }' ~/.tool-versions
+}
+
+function zsh_plugin:asdf:ensure () {
+  plugin=${1:-}
+  asdf plugin add "$1" &>/dev/null
+  # asdf current "$1" || asdf install "$1"
+}
 
 if [[ -d $ASDF_DIR ]]; then
   source $ASDF_DIR/asdf.sh
   fpath+=($ASDF_DIR/completions)
+
+  for plugin in $(zsh_plugin:asdf:tools); zsh_plugin:asdf:ensure $plugin
 fi
+
+unset zsh_plugin:asdf:ensure
+unset zsh_plugin:asdf:tools
