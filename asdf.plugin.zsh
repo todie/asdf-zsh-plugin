d2b77472df2c461a8d8f9ea0df5b0a57cf967006
Christian Todie <christian.todie@gmail.com>
2022-01-07 00:38:29 -0500

    fix: move env vars into plugin

--- asdf.plugin.zsh
+++ asdf.plugin.zsh
@@ -1,6 +1,8 @@
-asdf_dir="${asdf_dir:-$HOME/.asdf}"
+ASDF_DIR=$XDG_DATA_HOME/asdf
+ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdfrc
+ASDF_DATA_DIR=$XDG_CACHE_HOME/asdf
 
-if [[ -d $asdf_dir ]]; then
-  source $asdf_dir/asdf.sh
-  fpath+=(${ASDF_DIR}/completions)
+if [[ -d $ASDF_DIR ]]; then
+  source $ASDF_DIR/asdf.sh
+  fpath+=($ASDF_DIR/completions)
 fi
